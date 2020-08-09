12b552e407b50f9914c7eb0ed1979ac69863badb
zjianfa <zjianfa@foxmail.com>
2020-08-09 22:38:33 +0800

    增加初始化函数

--- src/main/java/algorithm/datastructure/ListNode.java
+++ src/main/java/algorithm/datastructure/ListNode.java
@@ -6,5 +6,31 @@ public class ListNode<T> {
 
     public ListNode(T val) {
         this.val = val;
+        this.next = null;
+    }
+
+    public static <T>ListNode<T> getListNode(T[] list){
+        if(list.length == 0){
+            return null;
+        }
+        ListNode head = new ListNode(-1);
+        ListNode curr = head;
+        for(int i = 0; i < list.length; i++){
+            curr.next = new ListNode<T>(list[i]);
+            curr = curr.next;
+        }
+        return head.next;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        ListNode curr = this;
+        while(curr != null){
+            sb.append(curr.val + " ");
+            curr = curr.next;
+        }
+        sb.append("null");
+        return sb.toString();
     }
 }
--- src/main/java/algorithm/datastructure/TreeNode.java
+++ src/main/java/algorithm/datastructure/TreeNode.java
@@ -1,5 +1,10 @@
 package algorithm.datastructure;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class TreeNode {
     public int val;
     public TreeNode left;
@@ -7,5 +12,58 @@ public class TreeNode {
 
     public TreeNode(int val){
         this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+
+    /**
+     * 建立二叉树
+     * @param list
+     * @return
+     */
+    public static TreeNode getTreeNode(List<Integer> list){
+        if(list ==null || list.size() == 0){
+            return null;
+        }
+        int index = 0;
+        int len = list.size();
+        TreeNode root = new TreeNode(list.get(index++));
+        Queue<TreeNode> queue = new LinkedList<>();      
+        queue.offer(root);
+        
+        while(index < len){
+            TreeNode node = queue.poll();
+            if(index < len && list.get(index) != null){
+                node.left = new TreeNode(list.get(index++));
+                queue.offer(node.left);
+            }else{
+                index += 1;
+            }
+            if(index < len && list.get(index) != null){
+                node.right = new TreeNode(list.get(index++));
+                queue.offer(node.right);
+            }else{
+                index += 1;
+            }
+        }
+        return root;
+    }
+
+    public static TreeNode getTreeNode(String[] list){
+        List<Integer> listArr = new ArrayList<>();
+        for(String s: list){
+            if(s == "null"){
+                listArr.add(null);
+            }else{
+                listArr.add(Integer.valueOf(s));
+            }
+        }
+        return getTreeNode(listArr);
+    }
+
+
+    @Override
+    public String toString() {
+        return "[val=" + val + ", left=" + left + ", right=" + right + "]";
     }
 }
